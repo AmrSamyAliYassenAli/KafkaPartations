d534499fffecce824075877b1d3809fa2d3de635
amr.samy <amr.samy@servicenow.com>
2025-01-09 00:15:48 +0200

    z

--- KafkaPartations/obj/Debug/net8.0/KafkaPartations.AssemblyInfo.cs
+++ KafkaPartations/obj/Debug/net8.0/KafkaPartations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KafkaPartations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558795fff4f159bf6b3253b5c72f3a056dffd08b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef4e7c3c0d12367143c70403c5986d89a634036")]
 [assembly: System.Reflection.AssemblyProductAttribute("KafkaPartations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KafkaPartations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
